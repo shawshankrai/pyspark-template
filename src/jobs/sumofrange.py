94c5e27042030f832bc6e995ee7d1cfb225a87cb
shawshankrai <shashank.xx92@gmail.com>
2022-07-09 18:16:42 +0530

    removed compilation issues

--- src/jobs/sumofrange.py
+++ src/jobs/sumofrange.py
@@ -1,6 +1,5 @@
-from sparkutils import get_spark_session
+from utils import sparkutils
 
 def handler():
-    spark = get_spark_session()
-    sum_of_range = spark.range(10000).where('id > 1000').selectExpr('sum(id)').collect()
-    print(sum_of_range)
+    spark = sparkutils.get_spark_session()
+    return spark.range(10000).where('id > 1000').selectExpr('sum(id)').collect()
--- src/main.py
+++ src/main.py
@@ -1,15 +1,13 @@
 import os
 import sys
+from jobs import sumofrange
 
 if os.path.exists('jobs.zip'):
     sys.path.insert(0, 'jobs.zip')
 else:
     sys.path.insert(0, './jobs')
     
-from jobs import sumofrange
 
 if __name__ == '__main__':
-    
-sum = sumofrange.handler()
-print(f'Sum of range is = {sum}')
+    print(f'Sum of range is = {sumofrange.handler()}')
     
